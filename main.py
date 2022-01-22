b6be7f58afff174955181f8eb45a2b81574513da
Kin <50994541+dotKinevi@users.noreply.github.com>
2022-01-22 14:01:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from functions import *
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-kinevion = Bot()
+class Main:
+	def __init__(self):
+		self.catusbot = Bot()
 
-kinevion.get_img()
-kinevion.post()
-kinevion.delete_used()
+	def post(self):
+		self.catusbot.get_img()
+		self.catusbot.post()
+		self.catusbot.delete_used()
 
+	def run(self):
+		posting = BlockingScheduler(timezone="Etc/GMT-3")
+		posting.add_job(self.post, 'interval', hours = 1)
+		posting.start()
 
-def post():
-	kinevion.get_img()
-	kinevion.post()
-	kinevion.delete_used()
-
-posting = BlockingScheduler(timezone="Etc/GMT-3")
-posting.add_job(post, 'interval', hours = 1)
-posting.start()
+if __name__ == "__main__":
+	main = Main()
+	main.post()
+	main.run()
